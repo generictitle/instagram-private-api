ba7a1c2bb84158e0ab785b61715d1539d7cc1fb4
Paul Sharratt <paul@rivuu.com>
2019-02-16 15:31:21 +1100

    cookie

--- src/v1/cookie-firebase-storage.js
+++ src/v1/cookie-firebase-storage.js
@@ -3,7 +3,7 @@ var FirebaseCookieStore = require('tough-cookie-firebasestore');
 const CookieStorage = require('./cookie-storage');
 
 class CookieFirebaseStorage extends CookieStorage {
-  constructor () {
+  constructor (app,firebasePath) {
     super(new FirebaseCookieStore(app,firebasePath));
   }
 
